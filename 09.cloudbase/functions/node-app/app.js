0075698521d2d5f9b4bd1147bb92e6ff83d6ff69
custer-go <custertian@163.com>
2021-05-16 21:41:35 +0800

    CloudBase 增删改查接口

--- 09.cloudbase/functions/node-app/app.js
+++ 09.cloudbase/functions/node-app/app.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
+const bodyparser = require('koa-bodyparser');
 const routing = require('./router');
 
 const app = new Koa();
 
+app.use(bodyparser());
 routing(app);
 
 module.exports = app;
--- 09.cloudbase/functions/node-app/controller/todo.js
+++ 09.cloudbase/functions/node-app/controller/todo.js
@@ -9,17 +9,48 @@ class TodoCtl {
 
   // 添加任务
   async create(ctx) {
-    ctx.body = '添加任务';
+    const { title } = ctx.request.body;
+    if (title == undefined) {
+      return;
+    }
+    let todo = {
+      title: ctx.request.body.title,
+      createTime: Date.now(),// 云函数的运行环境内保持的是 UTC 时间，即 0 时区时间，和北京时间有 8 小时的时间差。
+      // https://docs.cloudbase.net/cloud-function/timezone.html
+      done: false
+    }
+    let baskdata = await db.collection('todo').add(todo);
+    ctx.body = baskdata;
   }
 
   // 修改任务
   async update(ctx) {
-    ctx.body = '修改任务';
+    if (ctx.params.id == undefined) {
+      ctx.body = '缺少id';
+    }
+    let todoDone = false;
+    let { done } = ctx.request.body;
+    if (done == undefined) {
+      return;
+    } else if (done == 1) {
+      todoDone = true;
+    }
+
+    let todo = {
+      title: ctx.request.body.title,
+      done: todoDone
+    }
+    let backdata = await db.collection('todo').doc(ctx.params.id).update(todo);
+    ctx.body = backdata;
   }
 
   // 删除任务
   async delete(ctx) {
-    ctx.body = '删除任务';
+    if (ctx.params.id == undefined) {
+      ctx.body = '缺少id';
+    }
+    let backdata = await db.collection('todo').doc(ctx.params.id).remove();
+    ctx.body = backdata;
   }
 }
 
