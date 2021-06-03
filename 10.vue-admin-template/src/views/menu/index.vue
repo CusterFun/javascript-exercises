a9fce2d0dc4f04db7ea80a31c16f14ecd1dec41b
custer-go <custertian@163.com>
2021-06-03 20:46:55 +0800

    vue-admin-template 创建菜单列表接口和封装调用API方法

--- 10.vue-admin-template/src/views/menu/index.vue
+++ 10.vue-admin-template/src/views/menu/index.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
-    菜单管理
+    菜单管理 {{ list }}
   </div>
 </template>
 
 <script>
+import api from '@/api/menu'
+
 export default {
-  name: 'Menu'
+  name: 'Menu',
+  data() {
+    return {
+      list: [], // 列表数据
+      query: {} // 查询条件
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      const { data } = await api.getList(this.query)
+      this.list = data
+    }
+  }
 }
 </script>
 
