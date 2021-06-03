df6da1df918358046aed32b4bfc6587cec239b80
custer-go <custertian@163.com>
2021-06-03 23:24:46 +0800

    vue-admin-template 开发分页条件查询角色列表视图模板

--- 10.vue-admin-template/src/views/role/index.vue
+++ 10.vue-admin-template/src/views/role/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <div>
-    角色管理 {{ page.total }} <br>
-    {{ list }}
+  <div class="app-container">
+    <el-form :inline="true" :model="query" size="mini">
+      <el-form-item label="角色名称: ">
+        <el-input v-model.trim="query.name" />
+      </el-form-item>
+      <el-form-item>
+        <el-button icon="el-icon-search" type="primary" @click="queryData">查询</el-button>
+        <el-button icon="el-icon-refresh" @click="reloadData">重置</el-button>
+        <el-button icon="el-icon-circle-plus-outline" type="primary" @click="openAdd">新增</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table
+      :data="list"
+      border
+      stripe
+      style="width: 100%"
+    >
+      <el-table-column align="center" type="index" label="序号" width="50" />
+      <el-table-column align="center" prop="name" label="角色名称" />
+      <el-table-column align="center" prop="remark" label="备注" />
+      <el-table-column align="center" label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" size="mini" @click="handlerPermission(scope.row.id)">分配权限</el-button>
+          <el-button type="success" size="mini" @click="handlerEdit(scope.row.id)">编辑</el-button>
+          <el-button type="danger" size="mini" @click="handlerDelete(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      :current-page="page.current"
+      :page-sizes="[10, 20, 50]"
+      :page-size="page.size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="page.total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
@@ -29,6 +65,33 @@ export default {
       const { data } = await api.getList(this.query, this.page.current, this.page.size)
       this.list = data.records
       this.page.total = data.total
+    },
+    handlerPermission(id) {},
+    handlerEdit(id) {},
+    handlerDelete(id) {},
+    // val 是切换之后每页显示多少条数据
+    handleSizeChange(val) {
+      this.page.size = val
+      this.fetchData()
+    },
+    // 当页码改变后触发此方法，val 是当前点击或输入的页码
+    handleCurrentChange(val) {
+      this.page.current = val
+      this.fetchData()
+    },
+    // 条件查询
+    queryData() {
+      this.page.current = 1
+      this.fetchData()
+    },
+    // 重置条件查询
+    reloadData() {
+      this.query = {}
+      this.fetchData()
+    },
+    // 打开窗口，新增角色
+    openAdd() {
+
     }
   }
 }
