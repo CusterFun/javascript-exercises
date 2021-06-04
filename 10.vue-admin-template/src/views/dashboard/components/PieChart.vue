825d35ace785b3c2dc74b2dab0ebde15db3b826e
custer-go <custertian@163.com>
2021-06-04 23:45:20 +0800

    vue-admin-template 各技术频道文章统计-封装Echars饼状图组件

---
+++ 10.vue-admin-template/src/views/dashboard/components/PieChart.vue
@@ -0,0 +1,101 @@
+<template>
+  <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+  <div ref="main" :style="{width: width, height: height}" :class="className" />
+</template>
+
+<script>
+import * as echarts from 'echarts'
+require('echarts/theme/macarons') // 引入主题
+
+export default {
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '400px'
+    },
+    legendData: { // 左侧组件接收的数据
+      type: Array,
+      default: () => ['javascript', 'python', 'golang', 'rust']
+    },
+    seriesData: { // 饼状图中接收的数据
+      type: Array,
+      default: () => [
+        { value: 500, name: 'javascript' },
+        { value: 100, name: 'python' },
+        { value: 300, name: 'golang' },
+        { value: 200, name: 'rust' }
+      ]
+    }
+  },
+  data() {
+    return {
+      chart: null // 接收 echarts 实例
+    }
+  },
+  mounted() {
+    // 当加载dom后,会调用 this.initChart() 来进行初始化 echarts 实例
+    this.$nextTick(() => {
+      this.initChart()
+    })
+  },
+  // 当组件销毁之前会调用此钩子
+  beforeDestroy() {
+    if (!this.chart) {
+      return // echart 实例为空直接返回
+    }
+    this.chart.dispose() // 注销
+    this.chart = null // 置为null
+  },
+  methods: {
+    // 关于图表的初始化
+    initChart() {
+      // 基于准备好的dom, 初始化 echarts 实例, 第二个参数可以指定引用的主题
+      this.chart = echarts.init(this.$refs.main, 'macarons')
+
+      // 图表配置项
+      this.chart.setOption({
+        title: { // 标题
+          text: '各个技术能力',
+          subtext: '纯属虚构',
+          left: 'center'
+        },
+        tooltip: { // 鼠标放上去的提示框格式
+          trigger: 'item',
+          backgroundColor: '#fff'
+        },
+        legend: { // 左上角
+          orient: 'vertical', // 垂直排列
+          left: 'left' // 位置
+        },
+        series: [ // 序列: 展示的具体数据
+          {
+            name: '统计内容',
+            type: 'pie', // 饼状图
+            radius: '55%', // 饼图的大小
+            center: ['50%', '50%'], // 饼图位置[左,上]
+            data: this.seriesData, // 饼图数据
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10, // 图形阴影的模糊大小
+                shadowOffsetX: 0, // 阴影水平方向偏移距离
+                shadowColor: 'rgba(0, 0, 0, 0.5)' // 阴影颜色
+              }
+            }
+          }
+        ]
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- 10.vue-admin-template/src/views/dashboard/index.vue
+++ 10.vue-admin-template/src/views/dashboard/index.vue
@@ -1,62 +1,26 @@
 <template>
   <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
-  <div id="main" ref="main" style="width: 600px; height: 400px;" class="dashboard-container" />
+  <div class="dashboard-container">
+    <el-row :gutter="40">
+      <el-col :xs="24" :sm="24" :lg="12">
+        <el-card>
+          <pie-chart />
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
-
 <script>
-import api from '@/api/test'
-import * as echarts from 'echarts'
+import PieChart from './components/PieChart'
 
 export default {
+  name: 'Dashboard',
+  components: { PieChart },
   data() {
-    return {
-      name: {}
-    }
-  },
-  created() {
-    this.fetchData()
-  },
-  mounted() {
-    this.showMain()
-  },
-  methods: {
-    fetchData() {
-      api.test().then((response) => {
-        console.log('response: ')
-        this.name = response
-      })
-    },
-    showMain() {
-      // 基于准备好的dom，初始化echarts实例
-      // var myChart = echarts.init(document.getElementById('main'))
-      // var myChart = echarts.init(this.$refs['main'])
-      var myChart = echarts.init(this.$refs.main)
-
-      // 绘制图表
-      myChart.setOption({ // 使用指定的配置项和数据显示图表
-        title: { // 标题
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {
-          // 提示内容
-          // show: false // 是否显示提示框
-        },
-        legend: { // 图例组件，点击后会显示或隐藏柱状
-          data: ['销量']
-        },
-        xAxis: { // x 轴配置
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [{ // 序列
-          name: '销量', // 用于提示框中显示的内容
-          type: 'bar', // 指定使用哪个类型图表
-          data: [5, 20, 36, 10, 10, 20]
-        }]
-      })
-    }
+    return {}
   }
 }
+
 </script>
 
 <style lang="scss" scoped>
